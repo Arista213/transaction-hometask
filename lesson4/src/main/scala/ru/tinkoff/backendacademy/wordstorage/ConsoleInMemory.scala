766a4bf88756ffdaabea0edb01802beaca41c62a
Andrey Golikov <andrey@golikov.net>
2021-10-19 19:39:45 +0300

    Format codebase

--- lesson4/src/main/scala/ru/tinkoff/backendacademy/wordstorage/ConsoleInMemory.scala
+++ lesson4/src/main/scala/ru/tinkoff/backendacademy/wordstorage/ConsoleInMemory.scala
@@ -6,6 +6,8 @@ import ru.tinkoff.backendacademy.wordstorage.console.WordStorageConsoleFrontend
 import java.nio.file.Files
 
 object ConsoleInMemory extends App {
-  new WordStorageConsoleFrontend(new FileWordRepository(Files.createTempFile("backendacademy", null))).start()
+  new WordStorageConsoleFrontend(
+    new FileWordRepository(Files.createTempFile("backendacademy", null))
+  ).start()
 //  new WordStorageConsoleFrontend(new InMemoryWordRepository()).start()
 }
--- lesson4/src/main/scala/ru/tinkoff/backendacademy/wordstorage/console/WordStorageConsoleFrontend.scala
+++ lesson4/src/main/scala/ru/tinkoff/backendacademy/wordstorage/console/WordStorageConsoleFrontend.scala
@@ -13,12 +13,15 @@ class WordStorageConsoleFrontend(backend: WordRepository) {
     while (true) {
       val str = readLine
       str match {
-        case commandAndWord("get", word) => println(backend.get(word))
-        case commandAndWord("put", word) => backend.put(word)
+        case commandAndWord("get", word)    => println(backend.get(word))
+        case commandAndWord("put", word)    => backend.put(word)
         case commandAndWord("delete", word) => backend.delete(word)
-        case "quit" => return
-        case _ => println("Could not recognize command, please use one of " +
-          """"get", "put" or "delete" and then write your word, or write "quit"""")
+        case "quit"                         => return
+        case _ =>
+          println(
+            "Could not recognize command, please use one of " +
+              """"get", "put" or "delete" and then write your word, or write "quit""""
+          )
       }
     }
 }
